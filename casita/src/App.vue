558ae38eb01d9341e7f068a1ec3027cfd9d5e9a1
willyHere1938 <157993624+willyHere1938@users.noreply.github.com>
2025-03-05 04:26:58 +0000

    feat: Se crea el menu y se actualiza el codigo fuente

---
+++ casita/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import MenuPrincipal from './components/Menu.vue'
+</script>
+
+<template>
+  <div class="container-fluid">
+
+    <MenuPrincipal/>
+    <HelloWorld/>
+    
+  </div>
+  
+</template>
+
+<style scoped>
+
+</style>
---
+++ casita/src/components/Menu.vue
@@ -0,0 +1,52 @@
+<!-- sirve para todo el codigo html-->
+<template>
+
+
+    <nav class="navbar navbar-expand-lg navbar navbar-dark bg-dark">
+        <div class="container-fluid">
+
+            <a class="navbar-brand" href="https://www.google.com">casita</a>
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">inicio</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">contactos</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">productos</a>
+                    </li>
+
+                </ul>
+                <form class="d-flex">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+            </div>
+        </div>
+    </nav>
+
+
+</template>
+
+
+<!-- logica-->
+<script>
+
+</script>
+
+
+
+
+<!--diseño, si se coloca scope solo servira en el componente-->
+<style scope></style>
