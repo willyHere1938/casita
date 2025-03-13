4e48f9dabb3808e39894657f9e8de5813b1b70f1
willyHere1938 <157993624+willyHere1938@users.noreply.github.com>
2025-03-13 12:31:30 +0000

    feat

---
+++ casita/src/components/Footer.vue
@@ -0,0 +1,8 @@
+<footer style="
+    text-align: center;
+    padding: 20px;
+    background: url('ruta-de-tu-imagen.jpg') no-repeat center center;
+    background-size: cover;
+    color: white;">
+    <p>&copy; <?php echo date("Y"); ?> Mi Proyecto. Todos los derechos reservados.</p>
+</footer>
